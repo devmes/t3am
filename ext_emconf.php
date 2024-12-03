2afa069885347f9b81874f9cf7b45a5514c63586
Devid Messner <devidmessner@MacBook-Pro-von-Devid-Messner.local>
2024-12-03 11:50:26 +0100

    feat: first update to TYPO3 v13

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF['t3am'] = [
     'version' => '5.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.3.0-12.4.99',
+            'typo3' => '12.3.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
